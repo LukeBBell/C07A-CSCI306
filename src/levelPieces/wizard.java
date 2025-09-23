27980bc707a26fd3f61f670c7238d11d96c27d19
LukeBBell <LBell_@outlook.com>
2025-09-23 14:33:01 -0600

    wizard

--- src/levelPieces/wizard.java
+++ src/levelPieces/wizard.java
@@ -1,1 +1,27 @@
-//long distance that hits you and potentially 50% stops you from moving 
+//if you are not within two spaces of the wizard each turn he will have a 20% chance to shoot a fireball at you and kill you
+
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+
+public class wizard extends GamePiece {
+    
+    public wizard(int location) {
+        super('D', "door", location);
+    }
+
+ @Override
+    public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+        // check if player is within 2 spaces of the wizard
+        if (Math.abs(playerLocation - this.getLocation()) <= 2) {
+            return InteractionResult.NONE;
+        } else {
+            // 20% chance to shoot fireball and kill player
+            if (Math.random() < 0.2) {  // 0.0–1.0, so < 0.2 means 20% chance
+                return InteractionResult.KILL;
+            }
+        }
+        return InteractionResult.NONE;
+    }
+}
