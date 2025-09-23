48ebf7fb1f1dcafea2e17c752fa5b95b98b06b05
Cade Shiveley <cade_shiveley@mines.edu>
2025-09-23 14:15:01 -0600

    testing for bomb

--- src/levelPieces/chest.java
+++ src/levelPieces/chest.java
@@ -1,1 +1,22 @@
-//interact when on the same space
+//if you land on 
+
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+
+public class chest extends GamePiece {
+    
+    public chest(int location) {
+        super('X', "bomb", location);
+    }
+
+    @Override
+    public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+//checks if player landed on the chest 
+        if (playerLocation == this.getLocation()) {
+            return InteractionResult.ADVANCE;
+        }
+        return InteractionResult.NONE;
+    }
+}
