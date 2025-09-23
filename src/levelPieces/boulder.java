1c24c9c79d636bb1e5a6af7b12c33c093484ddd0
Cade Shiveley <cade_shiveley@mines.edu>
2025-09-23 14:24:56 -0600

    adding boulder class

--- src/levelPieces/boulder.java
+++ src/levelPieces/boulder.java
@@ -1,1 +1,22 @@
-//moves one space to the right each turn chasing you down
+//
+
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+
+public class boulder extends GamePiece {
+    
+    public boulder(int location) {
+        super('O', "boulder", location);
+    }
+
+    @Override
+    public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+//checks if player landed on the bomb
+        if (playerLocation == this.getLocation()) {
+            return InteractionResult.KILL;
+        }
+        return InteractionResult.NONE;
+    }
+}
