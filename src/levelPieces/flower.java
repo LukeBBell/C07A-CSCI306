dcd657e2ce355d60333263839b373626715cfc2e
LukeBBell <LBell_@outlook.com>
2025-09-23 13:53:39 -0600

    flower

--- src/levelPieces/flower.java
+++ src/levelPieces/flower.java
@@ -1,1 +1,27 @@
-//doesn't do anything
+
+
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.GameEngine;
+import gameEngine.InteractionResult;
+
+/**
+ *  This is an abstract class because it contains an abstract 
+ *  method AND an instance variable
+ * 
+ * @author Mark Baldwin
+ * @author Cyndi Rader
+ *
+ */
+public class flower extends GamePiece {
+    
+    public flower(int location) {
+        super('f', "flower", location);
+    }
+    
+    @Override
+    public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+        return InteractionResult.NONE;
+    }
+}
