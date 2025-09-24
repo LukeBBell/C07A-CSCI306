974225056e23c5693712e557f880dc23d3851d7a
LukeBBell <LBell_@outlook.com>
2025-09-24 17:10:24 -0600

    levelsetup get methods

--- src/levelPieces/LevelSetup.java
+++ src/levelPieces/LevelSetup.java
@@ -1,5 +1,39 @@
 package levelPieces;
 
+import gameEngine.Drawable;
+import java.util.ArrayList;
+import gameEngine.Moveable;
+
 public class LevelSetup {
     
+    Drawable[] board;
+    ArrayList<Moveable> movingPieces; 
+    ArrayList<GamePiece> interactingPieces;
+    int playerStartLoc;
+
+    public void createLevel(int levelNumber) {
+        // Implementation for setting up the level based on levelNumber
+        if 
+    }
+
+    public Drawable[] getBoard() {
+        // Return the game board array
+        return board;
+    }
+
+    public ArrayList<Moveable> getMovingPieces() {
+        // Return the list of moving pieces
+        return movingPieces;
+    }
+
+    public ArrayList<GamePiece> getInteractingPieces() {
+        // Return the list of interacting pieces
+        return interactingPieces;
+    }
+
+    public int getPlayerStartLoc() {
+        // Return the player's starting location
+        return playerStartLoc;
+    }
+
 }
