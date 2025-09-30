33d2ffc3db774dd237c070644ed4d3d312eefcd0
Cade Shiveley <cade_shiveley@mines.edu>
2025-09-29 19:37:17 -0600

    adding the rest of the interaction pieces

--- src/tests/TestInteractions.java
+++ src/tests/TestInteractions.java
@@ -22,4 +22,116 @@ public void testTrap(){
         assertEquals(InteractionResult.NONE, trap.InteractionResult(gameBoard, i));
     }
 }
+
+public void testChest(){
+    Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+    Chest chest= new chest(5);
+    gameBoard[5] = chest;
+    
+    assertEquals(InteractionResult.GET_POINT, trap.interact(gameBoard, 5));
+
+    for(int i=0; i<5; i++){
+        assertEquals(InteractionResult.NONE, chest.InteractionResult(gameBoard, i));
+    }
+    
+    for(int i=6; i<GameEngine.BOARD_SIZE; i++){
+        assertEquals(InteractionResult.NONE, chest.InteractionResult(gameBoard, i));
+    }
+}
+
+public void testDoor(){
+    Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+    Door door= new Door(5);
+    gameBoard[5] = door;
+    
+    assertEquals(InteractionResult.ADVANCE, door.interact(gameBoard, 5));
+
+    for(int i=0; i<5; i++){
+        assertEquals(InteractionResult.NONE, door.InteractionResult(gameBoard, i));
+    }
+    
+    for(int i=6; i<GameEngine.BOARD_SIZE; i++){
+        assertEquals(InteractionResult.NONE, door.InteractionResult(gameBoard, i));
+    }
+}
+
+public void test(){
+    Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+    Trap trap= new Trap(5);
+    gameBoard[5] = trap;
+    
+    assertEquals(InteractionResult.HIT, trap.interact(gameBoard, 5));
+
+    for(int i=0; i<5; i++){
+        assertEquals(InteractionResult.NONE, trap.InteractionResult(gameBoard, i));
+    }
+    
+    for(int i=6; i<GameEngine.BOARD_SIZE; i++){
+        assertEquals(InteractionResult.NONE, trap.InteractionResult(gameBoard, i));
+    }
+}
+
+public void testFlower(){
+    Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+    Flower flower= new Flower(5);
+    gameBoard[5] = flower;
+    
+    assertEquals(InteractionResult.NONE, flower.interact(gameBoard, 5));
+
+    for(int i=0; i<5; i++){
+        assertEquals(InteractionResult.NONE, flower.InteractionResult(gameBoard, i));
+    }
+    
+    for(int i=6; i<GameEngine.BOARD_SIZE; i++){
+        assertEquals(InteractionResult.NONE, flower.InteractionResult(gameBoard, i));
+    }
+}
+
+public void testGoblin(){
+    Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+    Goblin goblin= new Goblin(5);
+    gameBoard[5] = goblin;
+    
+    assertEquals(InteractionResult.HIT, goblin.interact(gameBoard, 5));
+
+    for(int i=0; i<5; i++){
+        assertEquals(InteractionResult.NONE, goblin.InteractionResult(gameBoard, i));
+    }
+    
+    for(int i=6; i<GameEngine.BOARD_SIZE; i++){
+        assertEquals(InteractionResult.NONE, goblin.InteractionResult(gameBoard, i));
+    }
+}
+
+public void testWizard(){
+    Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+    Wizard wizard= new Wizard(5);
+    gameBoard[5] = wizard;
+    
+    assertEquals(InteractionResult.KILL, wizard.interact(gameBoard, 5));
+
+    for(int i=0; i<5; i++){
+        assertEquals(InteractionResult.NONE, wizard.InteractionResult(gameBoard, i));
+    }
+    
+    for(int i=6; i<GameEngine.BOARD_SIZE; i++){
+        assertEquals(InteractionResult.NONE, wizard.InteractionResult(gameBoard, i));
+    }
+}
+
+public void testBoulder(){
+    Drawable[] gameBoard = new Drawable[GameEngine.BOARD_SIZE];
+    Boulder boulder= new Boulder(5);
+    gameBoard[5] = boulder;
+    
+    assertEquals(InteractionResult.KILL, boulder.interact(gameBoard, 5));
+
+    for(int i=0; i<5; i++){
+        assertEquals(InteractionResult.NONE, boulder.InteractionResult(gameBoard, i));
+    }
+    
+    for(int i=6; i<GameEngine.BOARD_SIZE; i++){
+        assertEquals(InteractionResult.NONE, boulder.InteractionResult(gameBoard, i));
+    }
+}
 }
